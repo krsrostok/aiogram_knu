ac3f11b150bb0d1f1fde6b3e49092c63d4ad63e5
krsrostok <128137066+krsrostok@users.noreply.github.com>
2023-05-25 00:23:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 
 API_TOKEN = '6133721144:AAEkwN_Xo6durT_aN25fJJ8MNsiVb8Wc-2s'
 
-CHANNELS = [
-        ["Переходник","-1001618536068","https://t.me/manga_perexodnik_ban"],
-        ["Mashle","-1001717656173","https://t.me/Magic_and_Muscles"],
-]
-NOT_SUB_MESSAGE = 'Для доступа к боту подпишись и нажми кнопку подписался'
+
